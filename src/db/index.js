079ba6ea30b47028ea51d952b61726feb486d254
XdMMD <102690832+MMDXd@users.noreply.github.com>
2024-02-11 13:21:30 +0330

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ const {join} = require("path")
 const {readdirSync} = require("node:fs")
 
 // connect to mongodb
-mongoose.connect(process.env.DB_PATH || process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/bookshop").then(() => console.log("Connected to Mongo DB")).catch(() => console.error("failed to connect to Mongo"))
+mongoose.connect(process.env.DB_PATH || process.env.DATABASE_URL || process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/bookshop").then(() => console.log("Connected to Mongo DB")).catch(() => console.error("failed to connect to Mongo"))
 
 
 // load schemas
