76a0da06eb4c5b982d77bc266120d40a3e3c7f5a
XdMMD <102690832+MMDXd@users.noreply.github.com>
2024-02-12 18:27:26 +0330

    Update login.js

--- src/routers/login.js
+++ src/routers/login.js
@@ -1,6 +1,6 @@
 const Router = require("express").Router()
 const {body, validationResult} = require("express-validator")
-const { checkUserPassword, setUserLogin } = require("../utils/auth")
+const { checkUserPassword, setUserLogin, getUserDataByEmail } = require("../utils/auth")
 
 
 const loginValidate = [
@@ -14,6 +14,9 @@ Router.post("/", loginValidate, async (req, res) => {
         return res.status(400).json({message: validate.array({onlyFirstError: true})[0]})
     }
     const {email, password} = req.body
+    if (!(await getUserDataByEmail(email)).valid) {
+        return res.json({valid: false})
+    }
     const login = await checkUserPassword(email, password)
     
     if (login.valid && setUserLogin(req, login.user._id)) {
